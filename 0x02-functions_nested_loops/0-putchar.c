ebe1a95241c8007d3eeabd47190ae3af8e34ff29
VickyBok6 <dungvictoria2017@gmail.com>
2022-12-13 12:40:26 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by Bok
+ * main - Entry point
  * Return: 0
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char c[0] = "_putchar";
+	int i = 0;
+
+	while (i < 0)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c)
+#ifndef MAIN_H_INCLUDE
+#define MAIN_H_ICLUDED 
+
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+void _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void time_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_time_table(int n);
 
 #endif
