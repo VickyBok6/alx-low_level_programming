eda29e59f5571704de4779f5776424cf5d162e35
VickyBok6 <dungvictoria2017@gmail.com>
2022-12-12 19:42:36 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Entry poin
+ * Return: Always (0) Success
  */
-
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
